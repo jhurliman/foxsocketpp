8b862b4fc401f070f4e82748d6daad10338b8fdf
John Hurliman <jhurliman@jhurliman.org>
2021-11-18 12:14:50 -0800

    Project skeleton

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <foxsocketpp.hpp>
+
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+bool running = true;
+
+void ReceiveSigInt(int sig) {
+  (void)sig;
+  running = false;
+}
+
+int main() {
+  std::cout << "foxsocketpp " FOXSOCKETPP_VERSION_STRING "\n";
+  foxsocketpp::FoxSocket server;
+  server.start();
+
+  signal(SIGINT, ReceiveSigInt);
+
+  while (running) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  }
+
+  server.stop();
+}
+
